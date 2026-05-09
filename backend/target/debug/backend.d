ed5e794467a65d6388156d450a1bc4c6dea42b32
f-fairy <terakura@mac.com>
2026-05-09 06:45:58 +0000

    Cleanup gitignore and remove target

--- backend/target/debug/backend.d
+++
@@ -1,1 +0,0 @@
-/workspaces/rich-time-card/backend/target/debug/backend: /workspaces/rich-time-card/backend/src/main.rs
--- backend/target/debug/deps/backend-8a428b998bcadbc2.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rich-time-card/backend/target/debug/deps/backend-8a428b998bcadbc2.d: src/main.rs
-
-/workspaces/rich-time-card/backend/target/debug/deps/backend-8a428b998bcadbc2: src/main.rs
-
-src/main.rs:
